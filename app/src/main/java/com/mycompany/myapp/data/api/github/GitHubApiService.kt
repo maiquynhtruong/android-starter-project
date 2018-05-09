f34a5762a87195890159547441ad9a07f38f7fe4
Mai Truong <maitruong2801@gmail.com>
2018-05-09 11:10:08 -0400

    Add listJobs call

--- app/src/main/java/com/mycompany/myapp/data/api/github/GitHubApiService.kt
+++ app/src/main/java/com/mycompany/myapp/data/api/github/GitHubApiService.kt
@@ -1,15 +1,22 @@
 package com.mycompany.myapp.data.api.github
 
 import com.mycompany.myapp.data.api.github.model.Commit
+import com.mycompany.myapp.data.api.github.model.Job
 
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GitHubApiService {
     @GET("repos/{user}/{repository}/commits")
     fun listCommits(
             @Path("user") user: String,
-            @Path("repository") repository: String): Single<Response<List<Commit>>>
+    @Path("repository") repository: String): Single<Response<List<Commit>>>
+
+    @GET("positions.json/search={keyword}&page=1")
+    fun listJobs(
+            @Query("keyword") keyword: String
+    ): Single<Response<List<Job>>>
 }
