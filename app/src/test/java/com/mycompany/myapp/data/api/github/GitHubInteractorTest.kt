c9c9827ca448f5b3b040b0f0773f420ecdcbadba
Mai Truong <maitruong2801@gmail.com>
2018-05-10 15:45:17 -0400

    testLoadJobs

--- app/src/test/java/com/mycompany/myapp/data/api/github/GitHubInteractorTest.kt
+++ app/src/test/java/com/mycompany/myapp/data/api/github/GitHubInteractorTest.kt
@@ -3,6 +3,7 @@ package com.mycompany.myapp.data.api.github
 import com.mycompany.myapp.data.api.github.GitHubInteractor.LoadCommitsRequest
 import com.mycompany.myapp.data.api.github.GitHubInteractor.LoadCommitsResponse
 import com.mycompany.myapp.data.api.github.model.CommitTestHelper.stubCommit
+import com.mycompany.myapp.data.api.github.model.JobTestHelper.stubJob
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
@@ -34,14 +35,39 @@ class GitHubInteractorTest {
         interactor = GitHubInteractor(context, api)
     }
 
+//    @Test
+//    @Throws(Exception::class)
+//    fun testLoadCommits() {
+//        val mockResponse = Single.just(Response.success(asList(stubCommit("test name", "test message"))))
+//        whenever(api.listCommits(anyString(), anyString())).thenReturn(mockResponse)
+//
+//        val subscriber = TestObserver<LoadCommitsResponse>()
+//        interactor.loadCommits(LoadCommitsRequest("user", "repo")).subscribeWith(subscriber)
+//        subscriber.await(1, TimeUnit.SECONDS)
+//
+//        subscriber.assertValueCount(1)
+//        subscriber.assertNoErrors()
+//        subscriber.assertComplete()
+//
+//        val response = subscriber.values()[0]
+//        assertEquals("user", response.request.user)
+//        assertEquals("repo", response.request.repository)
+//        assertEquals(1, response.commits.size.toLong())
+//
+//        val commit = response.commits[0]
+//        assertEquals("test name", commit.author)
+//        assertEquals("test message", commit.commitMessage)
+//    }
+
     @Test
     @Throws(Exception::class)
-    fun testLoadCommits() {
-        val mockResponse = Single.just(Response.success(asList(stubCommit("test name", "test message"))))
-        whenever(api.listCommits(anyString(), anyString())).thenReturn(mockResponse)
+    fun testLoadJobs() {
+        val mockResponse = Single.just(Response.success(asList(stubJob("Lead Software Architect",
+                "New York, NY", "Full TIme","The Expert Institute", "Bla"))))
+        whenever(api.listJobs(anyString(), anyString())).thenReturn(mockResponse)
 
-        val subscriber = TestObserver<LoadCommitsResponse>()
-        interactor.loadCommits(LoadCommitsRequest("user", "repo")).subscribeWith(subscriber)
+        val subscriber = TestObserver<GitHubInteractor.LoadJobsResponse>()
+        interactor.loadJobs(GitHubInteractor.LoadJobsRequest("mobile", "new+york")).subscribeWith(subscriber)
         subscriber.await(1, TimeUnit.SECONDS)
 
         subscriber.assertValueCount(1)
@@ -49,12 +75,16 @@ class GitHubInteractorTest {
         subscriber.assertComplete()
 
         val response = subscriber.values()[0]
-        assertEquals("user", response.request.user)
-        assertEquals("repo", response.request.repository)
-        assertEquals(1, response.commits.size.toLong())
+        assertEquals("mobile", response.request.keyword)
+        assertEquals("new+york", response.request.location)
+        assertEquals(1, response.jobs.size.toLong())
 
-        val commit = response.commits[0]
-        assertEquals("test name", commit.author)
-        assertEquals("test message", commit.commitMessage)
+        val job = response.jobs[0]
+        assertEquals("Lead Software Architect", job.title)
+        assertEquals("New York, NY", job.location)
+        assertEquals("Full TIme", job.type)
+        assertEquals("The Expert Institute", job.company)
+        assertEquals("Bla", job.description)
+
     }
 }
